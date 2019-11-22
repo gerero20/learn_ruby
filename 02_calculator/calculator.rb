d39a2d573899aee56a8460d27844560e35577ac3
Jonathan <parrottjonathanc@gmail.com>
2019-11-21 19:47:49 -0700

    initial commit with several exercises completed

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,30 @@
-#write your code here
+def add(a, b)
+  a + b
+end
+
+def subtract(a, b)
+  a - b
+end
+
+def sum(array)
+  array.inject(0, :+)
+end
+
+def multiply(array)
+  array.inject(:*)
+end
+
+def power(a, b)
+  a ** b
+end
+
+def factorial(num)
+  array = [num]
+  while num > 0
+    if num - 1 > 0
+      array.push(num - 1)
+    end
+    num -= 1
+  end
+  array.inject(:*)
+end
